ceaf5f80fa082142e1e790ccb45509a1161a63e5
Ycaro Rodrigues <112430708+ycaro98@users.noreply.github.com>
2022-12-13 11:09:11 -0300

    Salvar alterações do desafio completo

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,46 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class Usuario (val username: String, val matricula: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun addUsuario(){
+        for(usuario in inscritos){
+            println("nome: ${usuario.username}")
+        }
+    }
+    
+       fun listarInscritos() {
+        inscritos.forEach { u -> println("Nome: ${u.username} - Matrícula: ${u.matricula}") }
+    }
+       
+    fun printConteudoEducacional(){
+        for(cont in conteudos){
+            println("${cont.nome} - ${cont.duracao}min - (${cont.nivel})")
+        }
+    
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kotlinExperience = listOf(ConteudoEducacional("Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin", 120, Nivel.BASICO),
+  	  ConteudoEducacional("Estruturas de Controle de Fluxo e Coleções em Kotlin", 120, Nivel.BASICO))
+    	
+  		val formacaoKotlin = Formacao("Kotlin Experience", kotlinExperience, )
+        
+        formacaoKotlin.matricular(Usuario("Ycaro Rodrigues", "553322"))
+        formacaoKotlin.listarInscritos()
+        formacaoKotlin.printConteudoEducacional()
+   		  
 }
